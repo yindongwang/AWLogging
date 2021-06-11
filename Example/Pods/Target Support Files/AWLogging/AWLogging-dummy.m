1603eda306fc6567a813d35a3dfbd715f9b15a55
Albert Wang <yindong_wang@163.com>
2021-06-11 11:52:43 +0800

    AWLogging init commit

---
+++ Example/Pods/Target Support Files/AWLogging/AWLogging-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_AWLogging : NSObject
+@end
+@implementation PodsDummy_AWLogging
+@end
---
+++ Example/Pods/Target Support Files/Pods-AWLogging_Tests/Pods-AWLogging_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AWLogging_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_AWLogging_Tests
+@end
